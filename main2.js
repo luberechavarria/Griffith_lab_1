ee51cbf484a0b1cce4e531f08a70a83cd798779b
Luber Echavarria <luberechavarria@gmail.com>
2021-03-06 13:09:32 +1000

    First commit

---
+++ main2.js
@@ -0,0 +1,5 @@
+var one = 5;
+var two = 5;
+var three = 5;
+var result = "Sum of numbers is:";
+console.log(result + " " + one + two + three);
---
+++ main2.ts
@@ -0,0 +1,6 @@
+let one:number = 5;
+let two:number = 5;
+let three:number = 5;
+let result: string = "Sum of numbers is:";
+
+console.log(result + " " + one + two + three)
